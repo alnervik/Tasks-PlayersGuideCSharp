968d075326474a2af6c5e208e9b3f05972decccd
Kalle Alnervik <k.alnervik@gmail.com>
2022-06-22 13:28:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Console.WriteLine("Lets check the area of your triangle.");
+Console.WriteLine("Please input the base:");
+string triangleBaseText = Console.ReadLine();
+int triangleBase = Convert.ToInt32(triangleBaseText);
+Console.WriteLine("Please input the height:");
+string triangleHeightText = Console.ReadLine();
+int triangleHeight = Convert.ToInt32(triangleHeightText);
+decimal triangleArea = (triangleBase * triangleHeight) / 2;
+Console.WriteLine("Hmm lets see.. the area is: " + triangleArea);
+*/
+// aaayyyy
+/*
+Console.WriteLine("How many eggs gathered today?");
+string totalAmountEggsText = Console.ReadLine();
+int.TryParse(totalAmountEggsText, out int totalAmountEggs);
+float eggsEachSister = totalAmountEggs / 4;
+int eggsForDuckBear = totalAmountEggs % 4;
+Console.WriteLine("The sisters get: " + eggsEachSister + " eggs each");
+Console.WriteLine("And the duckbear gets: " + eggsForDuckBear + " eggs"); */
